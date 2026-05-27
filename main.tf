8467ce17f3dc0b228c9300f58ffd84685df5329d
Kevin Osei <knokwaku@gmail.com>
2026-05-27 15:14:55 +0100

    configuring remote backend

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-remote-state-kno"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
